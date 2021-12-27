db6963600da771bafb88dcd04a52457cc9908789
Jonathan Vanegas <srquike@outlook.com>
2021-12-27 15:27:27 -0600

    Se agrego el modelo para las mascotas

---
+++ Care4Pets.Api/Models/PetModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Care4Pets.Api.Models
+{
+    public class PetModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Breed { get; set; }
+        public string Specie { get; set; }
+        public string Gender { get; set; }
+        public string Appearance { get; set; }
+        public bool Sterilized { get; set; }
+        public DateTime? DateOfSterilization { get; set; }
+        public string Notes { get; set; }
+    }
+}
