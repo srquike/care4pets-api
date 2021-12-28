21b18298a04facc356be8cf09141c42c5513b4ea
Jonathan Vanegas <srquike@outlook.com>
2021-12-27 21:13:06 -0600

    Se agregó el modelo para los medicamentos

---
+++ Care4Pets.Api/Models/MedicationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Care4Pets.Api.Models
+{
+    public class MedicationModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Presentation { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime? ManufacturingDate { get; set; }
+        public string BatchNumber { get; set; }
+        public float UnitPrice { get; set; } = 0.0F;
+        public float SalePrice { get; set; } = 0.0F;
+    }
+}
