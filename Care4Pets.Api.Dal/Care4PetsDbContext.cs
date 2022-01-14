fea523fc64f947804809001ee219f8c4c244ee88
Jonathan Vanegas <srquike@outlook.com>
2022-01-13 19:40:48 -0600

    Se creó y configuró el contexto de la base de datos

---
+++ Care4Pets.Api.Dal/Care4PetsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Care4Pets.Api.Dal
+{
+    class Care4PetsDbContext : DbContext
+    {
+        public Care4PetsDbContext() : base("name=Care4PetsDbConnectionString")
+        {
+
+        }
+    }
+}
